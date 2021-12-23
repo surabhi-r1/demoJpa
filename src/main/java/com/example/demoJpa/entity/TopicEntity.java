daa441cafa6f7be95cb25eea61886d5ee500273a
surabhi-r1 <surabhi@emproto.com>
2021-12-23 16:56:25 +0530

    test cases added for controller

--- src/main/java/com/example/demoJpa/entity/TopicEntity.java
+++ src/main/java/com/example/demoJpa/entity/TopicEntity.java
@@ -2,17 +2,41 @@ package com.example.demoJpa.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class TopicEntity {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TopicEntity that = (TopicEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
     @Id
     private String id;
     private String name;
+
+    @Override
+    public String toString() {
+        return "TopicEntity{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
     private String description;
     public TopicEntity() {
     }
 
-    public String getId() {
+    public String getId(String java) {
         return id;
     }
 
